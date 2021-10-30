057dbe71db42e6fd390b4a1b5b848b418e0b10e6
EvelinaAB <eweburdzel@gmail.com>
2021-10-30 11:03:26 +0200

    Ejemplo2
    Venatana+3 botones

--- Ejemplo1/src/ejemplo1/Ejemplo1.java
+++ Ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -14,31 +14,45 @@ import java.awt.event.*;
 
 public class Ejemplo1 extends JFrame implements ActionListener {
 
-    JButton boton1;
+    private JButton boton1, boton2, boton3;
 
     public Ejemplo1() {
 //Layout absoluto
         setLayout(null);
 //Tamaño de la ventana
-        setBounds(0, 0, 450, 350);
+        setBounds(0, 0, 350, 200);
 //Título
-        setTitle("Ejemplo 1: Botón");
+        setTitle("Ejemplo 2: Botones");
 //No redimensionable
         setResizable(false);
 //Cerrar proceso al cerrar la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-//Botón
-        boton1 = new JButton("Finalizar");
-        boton1.setBounds(300, 250, 100, 30);
+//Botones
+        boton1 = new JButton("1");
+        boton1.setBounds(10, 100, 90, 30);
         add(boton1);
         boton1.addActionListener(this);
+        boton2 = new JButton("2");
+        boton2.setBounds(110, 100, 90, 30);
+        add(boton2);
+        boton2.addActionListener(this);
+        boton3 = new JButton("3");
+        boton3.setBounds(210, 100, 90, 30);
+        add(boton3);
+        boton3.addActionListener(this);
 //Muestro JFrame (lo último para que lo pinte todo correctamanete)
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == boton1) {
-            System.exit(0);
+            setTitle("boton 1");
+        }
+        if (e.getSource() == boton2) {
+            setTitle("boton 2");
+        }
+        if (e.getSource() == boton3) {
+            setTitle("boton 3");
         }
     }
 
