58147d70d33926bd9a5942c515f1bae948e21c57
EvelinaAB <eweburdzel@gmail.com>
2021-11-09 07:57:29 +0100

    Disponer dos objetos de la clase JButton con las etiquetas: "carne" y "pescado"

--- Ejercicio1_PrimeraParte/src/ejercicio1_primeraparte/Ejercicio1_PrimeraParte.java
+++ Ejercicio1_PrimeraParte/src/ejercicio1_primeraparte/Ejercicio1_PrimeraParte.java
@@ -6,6 +6,7 @@
 package ejercicio1_primeraparte;
 
 import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -29,14 +30,26 @@ public class Ejercicio1_PrimeraParte {
         ventana.setResizable(false);
 
         JLabel e1 = new JLabel("Ana");
-        e1.setBounds(400, 20, 200, 70);
+        e1.setBounds(400, 20, 70, 50);
         ventana.add(e1);
         JLabel e2 = new JLabel("Jose");
-        e2.setBounds(400, 40, 400, 100);
+        e2.setBounds(400, 40, 70, 50);
         ventana.add(e2);
         JLabel e3 = new JLabel("Alberto");
-        e3.setBounds(400, 60, 600, 130);
+        e3.setBounds(400, 60, 70, 50);
         ventana.add(e3);
+        JButton boton1, boton2;
+        boton1 = new JButton("carne");
+
+        ventana.add(boton1);
+        boton1.setBounds(15, 90, 70, 50);
+
+        boton2 = new JButton("pescado");
+        boton2.setBounds(15, 155, 70, 50);
+        ventana.add(boton2);;
+
+        ventana.add(boton1);
+        ventana.add(boton2);
 
     }
 
