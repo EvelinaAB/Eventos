19d0b47b769202a0f48fe98acc41ea78f79676fc
EvelinaAB <eweburdzel@gmail.com>
2021-11-09 10:48:02 +0100

    Abrir ventana correspondiente al botón
    Disponer dos objetos de la clase JButton con las etiquetas: "carne" y "pescado", al presionarse cada botón abrir una nueva ventana que muestre la opción elegida, no permitiendo elegir otra opción mientras no se cierre la anterior.

--- Ejercicio1_PrimeraParte/src/ejercicio1_primeraparte/Ejercicio1_PrimeraParte.java
+++ Ejercicio1_PrimeraParte/src/ejercicio1_primeraparte/Ejercicio1_PrimeraParte.java
@@ -6,6 +6,8 @@
 package ejercicio1_primeraparte;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,43 +16,107 @@ import javax.swing.JLabel;
  *
  * @author ewelina
  */
-public class Ejercicio1_PrimeraParte {
+public class Ejercicio1_PrimeraParte extends JFrame implements ActionListener {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        JFrame ventana = new JFrame("Ejemplo de manejo de eventos");
-        ventana.setLayout(new FlowLayout());
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setSize(
-                1024, 800);
-        ventana.setVisible(
-                true);
-        ventana.setResizable(false);
+    /* Declaro variables */
+    private JButton boton1;
+    private JButton boton2;
+    private JLabel e1;
+    private JLabel e2;
+    private JLabel e3;
 
-        JLabel e1 = new JLabel("Ana");
-        e1.setBounds(400, 20, 70, 50);
-        ventana.add(e1);
-        JLabel e2 = new JLabel("Jose");
-        e2.setBounds(400, 40, 70, 50);
-        ventana.add(e2);
-        JLabel e3 = new JLabel("Alberto");
-        e3.setBounds(400, 60, 70, 50);
-        ventana.add(e3);
-        JButton boton1, boton2;
+    /* Constructor */
+    public Ejercicio1_PrimeraParte() {
+//Layout absoluto
+        setLayout(null);
+//Tamaño de la ventana
+        setBounds(10, 10, 1024, 800);
+//Título
+        setTitle("Ejercicio1_Primera_Parte");
+//No redimensionable
+        setResizable(false);
+//Cerrar proceso al cerrar la ventana
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+//Etiqueta
+        e1 = new JLabel("Ana");
+        e2 = new JLabel("Jose");
+        e3 = new JLabel("Alberto");
+
+        e1.setBounds(
+                15, 50, 195, 50);
+        e2.setBounds(
+                15, 80, 195, 50);
+        e3.setBounds(
+                15, 110, 195, 50);
+
+        add(e1);
+        add(e2);
+        add(e3);
+//Botón
         boton1 = new JButton("carne");
 
-        ventana.add(boton1);
-        boton1.setBounds(15, 90, 70, 50);
+        boton1.setBounds(
+                15, 150, 195, 50);
 
         boton2 = new JButton("pescado");
-        boton2.setBounds(15, 155, 70, 50);
-        ventana.add(boton2);;
 
-        ventana.add(boton1);
-        ventana.add(boton2);
+        boton2.setBounds(
+                15, 200, 195, 50);
 
+        add(boton1);
+
+        add(boton2);
+        /* Inicializo escuchador del botón */
+        boton1.addActionListener(this);
+        boton2.addActionListener(this);
+//Muestro JFrame (lo último para que lo pinte todo correctamanete)
+        setVisible(true);
     }
 
+    /* Método que implementa la acción del botón */
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == boton1) {
+            /* Inicializo y muestro la segunda pantalla con los argumentos this (JFrame) y true (modal) */
+            SegundaVentanaCarne secundaria = new SegundaVentanaCarne(this, true);
+            secundaria.setVisible(true);
+        }
+        if (e.getSource() == boton2) {
+            /* Inicializo y muestro la segunda pantalla con los argumentos this (JFrame) y true (modal) */
+            SegundaVentanaPescado secundaria = new SegundaVentanaPescado(this, true);
+            secundaria.setVisible(true);
+        }
+    }
+ 
+
+    public static void main(String[] args) {
+        new Ejercicio1_PrimeraParte();
+    }
 }
+
+//
+//    
+//
+//    /* Método que implementa la acción del botón */
+//    boton1.addActionListener ( 
+//        new ActionListener() {
+//           
+//
+//            @Override
+//        public void actionPerformed
+//        (ActionEvent e
+//        
+//            ) {
+//                SegundaVentanaCarne secundaria = new SegundaVentanaCarne(this, true);
+//            secundaria.setVisible(true);
+//        }
+//    }
+//
+//);
+//        
+//
+//
+//    @Override
+//        public void actionPerformed(ActionEvent e) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+
